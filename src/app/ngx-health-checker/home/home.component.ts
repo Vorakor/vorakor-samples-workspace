b13e29e9fa0a0807a038da40e710d8aee4f44d48
Shaine Berube <shaine@vorakortech.io>
2020-02-11 01:39:53 -0700

    Trying to integrate health checker into demo

--- src/app/ngx-health-checker/home/home.component.ts
+++ src/app/ngx-health-checker/home/home.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { INgxHcConfig, NgxHealthCheckerService } from 'ngx-health-checker';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnDestroy {
+    healthCheckerConfig: INgxHcConfig = {
+        defaultFrequency: 4 * 60,
+        maxFrequency: 6 * 60,
+        debug: true,
+        servers: [
+            {
+                id: 1,
+                url: '/api',
+                endpoints: [
+                    {
+                        id: 1,
+                        url: '/true',
+                        method: 'get',
+                        healthy: true,
+                        type: 'api',
+                    },
+                ],
+            },
+        ],
+    };
+    subs;
+    constructor(private ngxHcService: NgxHealthCheckerService) {
+        // this.subs = this.ngxHcService.initialized$.pipe(filter(i => i !== null)).subscribe(init => {
+        //   if (!init) {
+        //     this.ngxHcService.initializeHealthChecker(this.healthCheckerConfig);
+        //   }
+        // });
+    }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    ngOnDestroy() {
+        this.subs.unsubscribe();
+    }
 }
--- src/app/ngx-health-checker/ngx-health-checker.module.ts
+++ src/app/ngx-health-checker/ngx-health-checker.module.ts
@@ -1,16 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { NgxHealthCheckerModule as NgxHcModule, NgxHealthCheckerService } from 'ngx-health-checker';
 
-import { HomeComponent } from './home/home.component';
-import { NgxHealthCheckerRoutingModule } from './ngx-health-checker-routing.module';
 import { FutureFeaturesComponent } from './future-features/future-features.component';
-import { InnerWorkingsComponent } from './inner-workings/inner-workings.component';
 import { HeartbeatExampleComponent } from './heartbeat-example/heartbeat-example.component';
+import { HomeComponent } from './home/home.component';
+import { InnerWorkingsComponent } from './inner-workings/inner-workings.component';
+import { NgxHealthCheckerRoutingModule } from './ngx-health-checker-routing.module';
 import { WebSocketExampleComponent } from './web-socket-example/web-socket-example.component';
 
 @NgModule({
-    declarations: [HomeComponent, FutureFeaturesComponent, InnerWorkingsComponent, HeartbeatExampleComponent, WebSocketExampleComponent],
-    imports: [CommonModule, MDBBootstrapModule, NgxHealthCheckerRoutingModule],
+    declarations: [
+        HomeComponent,
+        FutureFeaturesComponent,
+        InnerWorkingsComponent,
+        HeartbeatExampleComponent,
+        WebSocketExampleComponent,
+    ],
+    imports: [CommonModule, MDBBootstrapModule, NgxHealthCheckerRoutingModule, NgxHcModule],
+    providers: [NgxHealthCheckerService],
 })
 export class NgxHealthCheckerModule {}
